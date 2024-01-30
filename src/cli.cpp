eb796a70430e7139f370dac76fdf008398ee9418
James Nock <jpnock@gmail.com>
2024-01-30 02:33:38 +0000

    Improve clarity of CLI arg parsing & output to pretty print file

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,52 +1,65 @@
 #include <cli.h>
 
-int parseCommandLineArgs(int argc, char **argv, std::string &source_path, std::string &output_path) {
-  std::string input = "";
+CommandLineArguments parseCommandLineArgs(int argc, char **argv)
+{
+    std::string input = "";
 
-  if ((argc <= 1) || (argv[argc - 1] == NULL) || (argv[argc - 1][0] == '-')) {
-    std::cerr << "No command line arguments were provided" << std::endl;
-    return 1;
-  }
-  else {
-    input = argv[argc - 1];
-  }
-
-  // Prevent opterr messages from being outputted.
-  opterr = 0;
-
-  // bin/c_compiler -S [source-file.c] -o [dest-file.s]
-  int opt;
-  while ((opt = getopt(argc, argv, "S:o:")) != -1) {
-    switch (opt) {
-    case 'S':
-      source_path = std::string(optarg);
-      break;
-    case 'o':
-      output_path = std::string(optarg);
-      break;
-    case '?':
-      if (optopt == 'S' || optopt == 'o') {
-        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-      }
-      else if (isprint(optopt)) {
-        fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-      }
-      else {
-        fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
-      }
-      return 1;
+    if ((argc <= 1) || (argv[argc - 1] == NULL) || (argv[argc - 1][0] == '-'))
+    {
+        std::cerr << "No command line arguments were provided" << std::endl; 
+        exit(1);
     }
-  }
+    else
+    {
+        input = argv[argc - 1];
+    }
 
-  if (source_path.length() == 0) {
-    std::cerr << "The source path -S argument was not set." << std::endl;
-    return 1;
-  }
 
-  if (output_path.length() == 0) {
-    std::cerr << "The output path -o argument was not set." << std::endl;
-    return 1;
-  }
+    // Prevent opterr messages from being outputted.
+    opterr = 0;
 
-  return 0;
+    // ./bin/c_compiler -S [source-file.c] -o [dest-file.s]
+    CommandLineArguments cliArgs;
+    int opt;
+    while ((opt = getopt(argc, argv, "S:o:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'S':
+            cliArgs.compileSourcePath = std::string(optarg);
+            break;
+        case 'o':
+            cliArgs.compileOutputPath = std::string(optarg);
+            break;
+        case '?':
+            if (optopt == 'S' || optopt == 'o')
+            {
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            }
+            else if (isprint(optopt))
+            {
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            }
+            else
+            {
+                fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+            }
+            fprintf(stderr, "Exiting due to failure to parse CLI args\n");
+            exit(2);
+        }
+    }
+
+    if (cliArgs.compileSourcePath.length() == 0)
+    {
+        std::cerr << "The source path -S argument was not set." << std::endl;
+        exit(2);
+    }
+
+    if (cliArgs.compileOutputPath.length() == 0)
+    {
+        std::cerr << "The output path -o argument was not set." << std::endl;
+        exit(2);
+    }
+
+    return cliArgs;
 }
--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,40 +1,65 @@
 #include <fstream>
 #include <iostream>
-#include <unistd.h>
 
 #include "cli.h"
 #include "ast.hpp"
 
 int main(int argc, char **argv)
 {
-  // Parse CLI arguments, to fetch the values of the source and output files.
-  std::string source_path = "";
-  std::string output_path = "";
-  if (parseCommandLineArgs(argc, argv, source_path, output_path)) {
-    return 1;
-  }
+    // Parse CLI arguments to fetch the source file to compile and the path to output to.
+    // This retrives [source-file.c] and [dest-file.s], when the compiler is invoked as follows:
+    // ./bin/c_compiler -S [source-file.c] -o [dest-file.s]
+    auto commandLineArguments = parseCommandLineArgs(argc, argv);
 
-  // Parse input and generate AST
-  Node* root = parseAST(source_path);
+    // Parse input and generate AST
+    std::cout << "Parsing: " << commandLineArguments.compileSourcePath << std::endl;
+    auto root = parseAST(commandLineArguments.compileSourcePath);
+    std::cout << "AST parsing complete" << std::endl;
 
-  // Open the output file in truncation mode (to overwrite the contents)
-  std::ofstream output;
-  output.open(output_path, std::ios::trunc);
+    if (root == nullptr)
+    {
+        // Check something was actually returned by parseAST().
+        std::cerr << "The root of the AST was a null pointer. Likely the root was never initialised correctly during parsing." << std::endl;
+        return 3;
+    }
 
-  // Emit assembler directives
-  // TODO these are just examples ones, make sure you understand the concept of directives and correct them
-  std::vector<std::string> directives = {"text", "globl f"};
-  for (auto directive : directives) {
-    output << "." << directive << "\n";
-  }
-  output << std::endl;
+    // Open the output files in truncation mode (to overwrite the contents)
+    std::ofstream compiledOutput, prettyPrintOutput;
+    compiledOutput.open(commandLineArguments.compileOutputPath, std::ios::trunc);
 
-  // Do actual compilation
-  Context context;
-  root->emitRISC(output, context);
+    auto prettyPrintOutputPath = commandLineArguments.compileOutputPath + ".printed";
+    prettyPrintOutput.open(prettyPrintOutputPath, std::ios::trunc);
 
-  // Close output file
-  output.close();
+    // Emit assembler directives.
+    // TODO: these are just examples ones, make sure you understand
+    // the concept of directives and correct them. They likely should
+    // be emitted elsewhere in your compiler.
+    std::vector<std::string> directives = {"text", "globl f"};
+    for (auto directive : directives)
+    {
+        compiledOutput << "." << directive << "\n";
+    }
+    compiledOutput << std::endl;
 
-  return 0;
+    // Output the pretty print version of what was parsed to the .printed output
+    // file.
+    std::cout << "Printing parsed AST..." << std::endl;
+    root->print(prettyPrintOutput);
+    prettyPrintOutput.close();
+    std::cout << "Printed parsed AST to: " << prettyPrintOutputPath << std::endl;
+
+    // Create a Context. This can be used to pass around information about
+    // what's currently being compiled (e.g. function scope and variable names).
+    Context ctx;
+
+    // Compile from the root of the AST and output this to the compiledOutput
+    // file.
+    std::cout << "Compiling parsed AST..." << std::endl;
+    root->emitRISC(compiledOutput, ctx);
+    compiledOutput.close();
+    std::cout << "Compiled to: " << commandLineArguments.compileOutputPath << std::endl;
+
+    // Clean up afterwards.
+    delete root;
+    return 0;
 }
