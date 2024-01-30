3c2e12ce439503b44d71f8290c9df55445b948a6
James Nock <jpnock@gmail.com>
2024-01-30 01:48:57 +0000

    test: Improve output and logging of the test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -45,42 +45,43 @@ for DRIVER in compiler_tests/**/*_driver.c; do
     (( TOTAL++ ))
 
     TO_ASSEMBLE="${DRIVER%_driver.c}.c"
-    LOG_PATH="${TO_ASSEMBLE//\//_}"
+    LOG_PATH="${TO_ASSEMBLE#compiler_tests/}"
     LOG_PATH="./bin/output/${LOG_PATH%.c}"
+    BASE_NAME="$(basename "${LOG_PATH}")"
+    LOG_FILE_BASE="${LOG_PATH}/${BASE_NAME}"
+    rm -rf "${LOG_PATH}"
+    mkdir -p "${LOG_PATH}"
 
     echo "${TO_ASSEMBLE}"
     printf '%s\n' "<testcase name=\"${TO_ASSEMBLE}\">" >> "${J_UNIT_OUTPUT_FILE}"
 
-    OUT="${LOG_PATH}"
-    rm -f "${OUT}.s"
-    rm -f "${OUT}.o"
-    rm -f "${OUT}"
-    ./bin/c_compiler -S "${TO_ASSEMBLE}" -o "${OUT}.s" 2> "${LOG_PATH}.compiler.stderr.log" > "${LOG_PATH}.compiler.stdout.log"
+    OUT="${LOG_FILE_BASE}"
+    ./bin/c_compiler -S "${TO_ASSEMBLE}" -o "${OUT}.s" 2> "${LOG_FILE_BASE}.compiler.stderr.log" > "${LOG_FILE_BASE}.compiler.stdout.log"
     if [ $? -ne 0 ]; then
-        fail_testcase "Fail: see ${LOG_PATH}.compiler.stderr.log and ${LOG_PATH}.compiler.stdout.log"
+        fail_testcase "Failed to compile testcase: see ${LOG_FILE_BASE}.compiler.stderr.log and ${LOG_FILE_BASE}.compiler.stdout.log and ${OUT}.s"
         continue
     fi
 
-    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "${OUT}.o" -c "${OUT}.s" 2> "${LOG_PATH}.assembler.stderr.log" > "${LOG_PATH}.assembler.stdout.log"
+    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "${OUT}.o" -c "${OUT}.s" 2> "${LOG_FILE_BASE}.assembler.stderr.log" > "${LOG_FILE_BASE}.assembler.stdout.log"
     if [ $? -ne 0 ]; then
-        fail_testcase "Fail: see ${LOG_PATH}.assembler.stderr.log and ${LOG_PATH}.assembler.stdout.log"
+        fail_testcase "Failed to assemble: see ${LOG_FILE_BASE}.assembler.stderr.log and ${LOG_FILE_BASE}.assembler.stdout.log and ${OUT}.s"
         continue
     fi
 
-    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -static -o "${OUT}" "${OUT}.o" "${DRIVER}" 2> "${LOG_PATH}.linker.stderr.log" > "${LOG_PATH}.linker.stdout.log"
+    riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -static -o "${OUT}" "${OUT}.o" "${DRIVER}" 2> "${LOG_FILE_BASE}.linker.stderr.log" > "${LOG_FILE_BASE}.linker.stdout.log"
     if [ $? -ne 0 ]; then
-        fail_testcase "Fail: see ${LOG_PATH}.linker.stderr.log and ${LOG_PATH}.linker.stdout.log"
+        fail_testcase "Failed to link driver: see ${LOG_FILE_BASE}.linker.stderr.log and ${LOG_FILE_BASE}.linker.stdout.log and ${OUT}.s"
         continue
     fi
 
-    spike pk "${OUT}" > "${LOG_PATH}.simulation.log"
+    spike pk "${OUT}" > "${LOG_FILE_BASE}.simulation.log"
     if [ $? -eq 0 ]; then
         echo -e "\t> Pass"
         (( PASSING++ ))
 
         printf '%s\n' "</testcase>" >> "${J_UNIT_OUTPUT_FILE}"
     else
-        fail_testcase "Fail: simulation did not exit with exit-code 0"
+        fail_testcase "Failed to simulate: simulation did not exit with code 0, see ${LOG_FILE_BASE}.simulation.log and ${OUT}.s"
     fi
 done
 
