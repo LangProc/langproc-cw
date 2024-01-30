2d74cd2284ccea085412603fd7161543060069b8
James Nock <jpnock@gmail.com>
2024-01-30 23:10:13 +0000

    Improve Context docs

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -1,9 +1,12 @@
-#ifndef AST_CONTEXT
-#define AST_CONTEXT
+#ifndef AST_CONTEXT_HPP
+#define AST_CONTEXT_HPP
 
-// An object of class Context is passed between AST nodes during compilation to provide adequate context
-class Context {
-  /* TODO decide what goes inside here */
+// An object of class Context is passed between AST nodes during compilation.
+// This can be used to pass around information about what's currently being
+// compiled (e.g. function scope and variable names).
+class Context
+{
+    /* TODO decide what goes inside here */
 };
 
 #endif
