bc0b2295c824768d5979ac2d4047d8d190542ca9
James Nock <jpnock@gmail.com>
2024-01-30 23:09:12 +0000

    Add a helper class, NodeList, and demonstrate its usage

--- include/ast_node.hpp
+++ include/ast_node.hpp
@@ -6,15 +6,63 @@
 
 #include "ast_context.hpp"
 
-class Node {
+class Node
+{
 protected:
-  std::vector<Node*> branches;
+    std::vector<Node *> branches;
 
 public:
-  Node() {};
-  virtual ~Node();
-  virtual void emitRISC(std::ostream &stream, Context &context) const = 0;
-  virtual void print(std::ostream &stream) const = 0;
+    Node(){};
+    virtual ~Node();
+    virtual void emitRISC(std::ostream &stream, Context &context) const = 0;
+    virtual void print(std::ostream &stream) const = 0;
+};
+
+// Represents a list of nodes.
+class NodeList : public Node
+{
+private:
+    std::vector<Node *> nodes;
+
+public:
+    NodeList(Node *firstNode) : nodes({firstNode}) {}
+
+    ~NodeList()
+    {
+        for (auto node : nodes)
+        {
+            delete node;
+        }
+    }
+
+    inline void push_back(Node *item)
+    {
+        nodes.push_back(item);
+    }
+
+    virtual void emitRISC(std::ostream &stream, Context &context) const override
+    {
+        for (auto node : nodes)
+        {
+            if (node == nullptr)
+            {
+                continue;
+            }
+            node->emitRISC(stream, context);
+        }
+    }
+
+    virtual void print(std::ostream &stream) const override
+    {
+        for (auto node : nodes)
+        {
+            if (node == nullptr)
+            {
+                continue;
+            }
+            node->print(stream);
+        }
+    }
 };
 
 #endif
