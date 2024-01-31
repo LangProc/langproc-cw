ea18f8f5c8cd681e47c6535f6f38829444b78cb5
James Nock <jpnock@gmail.com>
2024-01-31 03:34:35 +0000

    Separate headers and implementation

--- include/ast_constant.hpp
+++ include/ast_constant.hpp
@@ -11,15 +11,8 @@ private:
 public:
     IntConstant(int value) : value_(value) {}
 
-    void EmitRISC(std::ostream &stream, Context &context) const override
-    {
-        stream << "li a0, " << value_ << std::endl;
-    }
-
-    void Print(std::ostream &stream) const override
-    {
-        stream << value_;
-    }
+    void EmitRISC(std::ostream &stream, Context &context) const override;
+    void Print(std::ostream &stream) const override;
 };
 
 #endif
