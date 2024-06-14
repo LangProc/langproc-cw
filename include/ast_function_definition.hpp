5ca63d24c2d39d65c5b31bc91d10df01bd9706c9
John Wickerson <j.wickerson@imperial.ac.uk>
2024-06-14 15:21:09 +0100

    adding comments to Makefile

--- include/ast_function_definition.hpp
+++ include/ast_function_definition.hpp
@@ -5,20 +5,32 @@
 
 class FunctionDefinition : public Node
 {
+  
 private:
+  
     Node *declaration_specifiers_;
     Node *declarator_;
     Node *compound_statement_;
 
 public:
-    FunctionDefinition(Node *declaration_specifiers, Node *declarator, Node *compound_statement) : declaration_specifiers_(declaration_specifiers), declarator_(declarator), compound_statement_(compound_statement){};
+  
+    FunctionDefinition
+    (Node *declaration_specifiers,
+     Node *declarator,
+     Node *compound_statement) :
+      declaration_specifiers_(declaration_specifiers),
+      declarator_(declarator),
+      compound_statement_(compound_statement){};
+  
     ~FunctionDefinition()
     {
         delete declaration_specifiers_;
         delete declarator_;
         delete compound_statement_;
     };
+  
     void EmitRISC(std::ostream &stream, Context &context) const override;
+  
     void Print(std::ostream &stream) const override;
 };
 
