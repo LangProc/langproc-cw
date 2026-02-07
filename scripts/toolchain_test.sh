ea85301ceaa5b01bdad60465d60dde32b93039ab
William Huynh <williamhuynh2002@gmail.com>
2026-02-07 14:22:33 +0000

    Rename compiler_tests/ to tests/ and move debugging/ to docs

--- scripts/toolchain_test.sh
+++ scripts/toolchain_test.sh
@@ -7,8 +7,8 @@ set -euo pipefail
 
 make build/c_compiler
 rm -f build/riscv_example.s
-./build/c_compiler -S "compiler_tests/_example/example.c" -o "build/riscv_example.s"
-riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "build/riscv_example" "build/riscv_example.s" "compiler_tests/_example/example_driver.c"
+./build/c_compiler -S "tests/_example/example.c" -o "build/riscv_example.s"
+riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "build/riscv_example" "build/riscv_example.s" "tests/_example/example_driver.c"
 
 set +e
 spike pk "build/riscv_example"
