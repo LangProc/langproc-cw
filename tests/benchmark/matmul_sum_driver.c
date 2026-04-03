7c39d43e3332974da37631cfdc0c15445b0e5528
Fiwo735 <fiwo725@gmail.com>
2026-04-03 22:12:56 +0000

    Moved rdinstret64 into benchmark.h

--- tests/benchmark/matmul_sum_driver.c
+++ tests/benchmark/matmul_sum_driver.c
@@ -1,19 +1,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <stdio.h>
-
-static inline uint64_t rdinstret64(void) {
-    uint32_t hi, lo, hi2;
-    asm volatile(
-        "1:\n"
-        "rdinstreth %0\n"
-        "rdinstret  %1\n"
-        "rdinstreth %2\n"
-        "bne %0, %2, 1b\n"
-        : "=&r"(hi), "=&r"(lo), "=&r"(hi2)
-    );
-    return ((uint64_t)hi << 32) | lo;
-}
+#include "benchmark.h"
 
 float matmul_sum(float A[5][5], float B[5][5]);
 
